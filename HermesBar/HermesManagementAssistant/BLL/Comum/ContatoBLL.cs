88e777a7a75cb21b457885009354591446a9cbff
unknown <giulianocosta@outlook.com>
2015-03-05 16:42:44 -0300

    correcao no cadastro de fornecedor

--- HermesBar/HermesManagementAssistant/BLL/Comum/ContatoBLL.cs
+++ HermesBar/HermesManagementAssistant/BLL/Comum/ContatoBLL.cs
@@ -27,9 +27,24 @@ namespace BLL.Comum
             if (Validacoes.ValidarEmail(contato.Email))
             {
                 if(!string.IsNullOrWhiteSpace(contato.Nome))
-                    return DAO.Salvar(contato);
+                    return DAO.Salvar(VerifyNullValues(contato));
             }  
             return null;
+        }
+
+        private ContatoModel VerifyNullValues(ContatoModel contato)
+        {
+            if (string.IsNullOrEmpty(contato.Celular))
+                contato.Celular = "";
+            if (string.IsNullOrEmpty(contato.Email))
+                contato.Email = "";
+            if (string.IsNullOrEmpty(contato.Nome))
+                contato.Nome = "";
+            if (string.IsNullOrEmpty(contato.Site))
+                contato.Site = "";
+            if (string.IsNullOrEmpty(contato.Telefone))
+                contato.Site = "";
+            return contato;
         }
         public int RecuperaProximoId()
         {
