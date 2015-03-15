079f090c36bef6d87a434ca14d7b53bd414e1f07
unknown <giulianocosta@outlook.com>
2015-03-15 01:46:01 -0300

    criacao do gerenciador de banco

---
+++ HermesBar/GerenciadorBancoHMA/GerenciadorBancoHMA/CriarBanco.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GerenciadorBancoHMA
+{
+    /// <summary>
+    /// Interaction logic for CriarBanco.xaml
+    /// </summary>
+    public partial class CriarBanco : Window
+    {
+        public CriarBanco()
+        {
+            InitializeComponent();
+        }
+        public void CriarBancoGeral(object sender, RoutedEventArgs e)
+        {
+            Connection conn = new Connection();
+            MessageBox.Show(conn.CriarBanco(tbNomeServidor.Text));
+            this.Close();
+        }
+    }
+}
