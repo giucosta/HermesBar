784f514967ab59cb6f1eb1296b26f85f840ec5a9
Giuliano Henrique Costa <giuliano.costa@nms.com.br>
2015-08-29 00:40:31 -0300

    inicio funcionario

---
+++ HermesBar/Web/HermesBarWEB/ENTITY/Employee/HMA_FUNC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Employee
+{
+    public class HMA_FUNC : RegControl
+    {
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string CTPS { get; set; }
+        public DateTime DT_ADM { get; set; }
+        public DateTime DT_DEM { get; set; }
+        public int TIP { get; set; }
+        public int _ID_CAR { get; set; }
+        public string FUN { get; set; }
+    }
+}
---
+++ HermesBar/Web/HermesBarWEB/ENTITY/Employee/HMA_TIP_FUNC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY.Employee
+{
+    public class HMA_TIP_FUNC : RegControl
+    {
+        public string TIP { get; set; }
+        public string DESCR { get; set; }
+    }
+}
