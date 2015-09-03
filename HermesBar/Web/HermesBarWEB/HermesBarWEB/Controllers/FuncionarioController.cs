53913048567325b7b89685fe060e93adf1bb5814
Giuliano Henrique Costa <giuliano.costa@nms.com.br>
2015-09-02 22:59:24 -0300

    controller e view funcionario

---
+++ HermesBar/Web/HermesBarWEB/HermesBarWEB/Controllers/FuncionarioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HermesBarWEB.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public ActionResult Get()
+        {
+            return View();
+        }
+    }
+}
