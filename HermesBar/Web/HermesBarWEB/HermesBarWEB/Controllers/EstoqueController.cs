579803ddbbed0d4f229142975dc48976e50e3e37
Giuliano Henrique Costa <giuliano.costa@nms.com.br>
2015-09-06 01:40:52 -0300

    inicio consulta estoque

---
+++ HermesBar/Web/HermesBarWEB/HermesBarWEB/Controllers/EstoqueController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HermesBarWEB.Controllers
+{
+    public class EstoqueController : Controller
+    {
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+    }
+}
