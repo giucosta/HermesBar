b3faee6d487da3a67284a122e79b6f29f23e2068
Giuliano Henrique Costa <giuliano.costa@nms.com.br>
2015-09-28 00:02:45 -0300

    inicio tela pedidos

---
+++ HermesBar/Web/HermesBarWEB/HermesBarWEB/Controllers/PedidoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HermesBarWEB.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public ActionResult Pedidos()
+        {
+            return View();
+        }
+    }
+}
